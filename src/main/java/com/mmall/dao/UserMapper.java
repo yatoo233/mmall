c82e71ab7954d4b423eb211bc80b2f00a38beff3
wangbl <yatoo233@outlook.com>
2019-07-03 09:47:34 +0800

    api

--- src/main/java/com/mmall/dao/UserMapper.java
+++ src/main/java/com/mmall/dao/UserMapper.java
@@ -14,4 +14,21 @@ public interface UserMapper {
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
+
+    int checkUsername(String username);
+
+    User selectLogin(String username,String password);
+
+    int checkEmail(String str);
+
+    String selectQuestionByUsername(String username);
+
+    int checkAnswer(String username,String question,String answer);
+
+    int updatePasswordByUsername(String username,String passwordNew);
+
+    int checkPassword(String password,int userId);
+
+    int checkEmailByUserId(String email,int userId);
+
 }
