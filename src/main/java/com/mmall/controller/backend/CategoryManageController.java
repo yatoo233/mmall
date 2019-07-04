f74f1214d922e3663dd8fef6f63ddafab86b02e6
wangbl <yatoo233@outlook.com>
2019-07-04 17:10:09 +0800

    user api

--- src/main/java/com/mmall/controller/backend/CategoryManageController.java
+++ src/main/java/com/mmall/controller/backend/CategoryManageController.java
@@ -9,6 +9,7 @@ import com.mmall.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 
@@ -24,19 +25,68 @@ public class CategoryManageController {
     @Autowired
     private ICategoryService iCategoryService;
 
-    @RequestMapping("/manage/category")
-    public ServerResponse<Integer> addCategory(HttpSession session,String categoryName,@RequestParam(value = "parentId",defaultValue = "0")int parentId){
+
+    @RequestMapping("add_category.do")
+    @ResponseBody
+    public ServerResponse addCategory(HttpSession session,String categoryName,@RequestParam(value = "parentId",defaultValue = "0") int parentId){
         User user = (User)session.getAttribute(Const.CURRENT_USER);
-        if(user==null){
-            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录");
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录");
         }
+        //校验一下是否是管理员
+        if(iUserService.checkAdminRole(user).isSuccess()){
+            //是管理员
+            //增加我们处理分类的逻辑
+            return iCategoryService.addCategory(categoryName,parentId);
 
-        if(!iUserService.checkAdminRole(user).isSuccess()){
-            return ServerResponse.createByErrorMessage("无管理员权限");
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作,需要管理员权限");
         }
-
-        return iCategoryService.insert(categoryName, parentId);
     }
 
+    @RequestMapping("setCategoryName.do")
+    @ResponseBody
+    public ServerResponse setCategoryName(HttpSession session,Integer categoryId,String categoryName){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录");
+        }
+        if(iUserService.checkAdminRole(user).isSuccess()){
+            //更新categoryName
+            return iCategoryService.updateCategoryName(categoryId,categoryName);
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作,需要管理员权限");
+        }
+    }
 
-}
+    @RequestMapping("get_category.do")
+    @ResponseBody
+    public ServerResponse getChildrenParallelCategory(HttpSession session,@RequestParam(value = "categoryId" ,defaultValue = "0") Integer categoryId){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录");
+        }
+        if(iUserService.checkAdminRole(user).isSuccess()){
+            //查询子节点的category信息,并且不递归,保持平级
+            return iCategoryService.getChildrenParallelCategory(categoryId);
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作,需要管理员权限");
+        }
+    }
+
+    @RequestMapping("get_deep_category.do")
+    @ResponseBody
+    public ServerResponse getCategoryAndDeepChildrenCategory(HttpSession session,@RequestParam(value = "categoryId" ,defaultValue = "0") Integer categoryId){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录");
+        }
+        if(iUserService.checkAdminRole(user).isSuccess()){
+            //查询当前节点的id和递归子节点的id
+            //0->10000->100000
+            return iCategoryService.selectCategoryAndChildrenById(categoryId);
+
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作,需要管理员权限");
+        }
+}}
--- src/main/java/com/mmall/service/ICategoryService.java
+++ src/main/java/com/mmall/service/ICategoryService.java
@@ -3,11 +3,21 @@ package com.mmall.service;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.Category;
 
+import java.util.List;
+import java.util.Set;
+
 /**
  * created by wangbl
  * 2019/7/3 5:21 PM
  */
 public interface ICategoryService {
 
-    ServerResponse<Integer> insert(String categoryName,int parentId);
+    ServerResponse addCategory(String categoryName,Integer parentId);
+
+    ServerResponse updateCategoryName(Integer categoryId,String categoryName);
+
+    ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId);
+
+    ServerResponse<List<Integer>> selectCategoryAndChildrenById(Integer categoryId);
+
 }
